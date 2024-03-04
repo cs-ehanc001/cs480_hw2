8931816e905c1b479c4ec0b0b28acf88f4d6ec7f
Ethan Hancock <ehanc001@odu.edu>
2024-03-04 16:07:41 -0500

    add trivial column move test

--- cpp/tests/src/sudoku/trivial_moves.cpp
+++ cpp/tests/src/sudoku/trivial_moves.cpp
@@ -62,11 +62,62 @@ static auto test_trivial_row_moves() -> supl::test_results
   return results;
 }
 
+static auto test_trivial_column_moves() -> supl::test_results
+{
+  supl::test_results results;
+
+  Sudoku trivial_column_moves {
+    {
+     // clang-format off
+  '_', '_', '_', '_', '_', '_', '_', '_', '_',
+  '7', '2', '5', '3', '4', '1', '6', '_', '_',
+  '3', '4', '6', '9', '7', '8', '2', '1', '5',
+  '9', '8', '1', '2', '5', '7', '4', '6', '3',
+  '5', '6', '4', '1', '3', '9', '8', '7', '2',
+  '2', '3', '7', '6', '8', '4', '1', '5', '9',
+  '4', '7', '3', '8', '1', '5', '9', '2', '6',
+  '8', '1', '9', '7', '6', '2', '5', '3', '4',
+  '6', '5', '2', '4', '9', '3', '7', '8', '1'
+     // clang-format on
+    }
+  };
+
+  for ( [[maybe_unused]] const int _ : std::views::iota(0, 8) ) {
+    results.enforce_true(trivial_column_moves.apply_trivial_move());
+  }
+
+  results.enforce_false(trivial_column_moves.apply_trivial_move());
+  results.enforce_true(trivial_column_moves.is_valid());
+  results.enforce_false(trivial_column_moves.is_solved());
+
+  const Sudoku expected_result {
+    {
+     // clang-format off
+  '1', '9', '8', '5', '2', '6', '3', '_', '_',
+  '7', '2', '5', '3', '4', '1', '6', '_', '_',
+  '3', '4', '6', '9', '7', '8', '2', '1', '5',
+  '9', '8', '1', '2', '5', '7', '4', '6', '3',
+  '5', '6', '4', '1', '3', '9', '8', '7', '2',
+  '2', '3', '7', '6', '8', '4', '1', '5', '9',
+  '4', '7', '3', '8', '1', '5', '9', '2', '6',
+  '8', '1', '9', '7', '6', '2', '5', '3', '4',
+  '6', '5', '2', '4', '9', '3', '7', '8', '1'
+     // clang-format on
+    }
+  };
+
+  const Sudoku& result = trivial_column_moves;
+  results.enforce_equal(result, expected_result);
+
+  return results;
+}
+
 static auto trivial_moves() -> supl::test_section
 {
   supl::test_section section;
 
   section.add_test("Trivial row moves", &test_trivial_row_moves);
+  section.add_test("Trivial column moves", &test_trivial_column_moves);
 
   return section;
 }
