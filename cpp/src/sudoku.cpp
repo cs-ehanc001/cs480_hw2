800a0907d4080d3c5b50e5aad51768c43638ae35
Ethan Hancock <ehanc001@odu.edu>
2024-03-03 20:18:36 -0500

    section checking with a dirty index hack

--- cpp/src/sudoku.cpp
+++ cpp/src/sudoku.cpp
@@ -1,5 +1,6 @@
 #include <array>
 #include <cassert>
+#include <cstddef>
 #include <ranges>
 
 #include <supl/utility.hpp>
@@ -96,6 +97,116 @@ static auto is_legal_state(const Sudoku& sudoku) noexcept -> bool
   // columns are good
 
   // check sections
+
+  // dirty indexing hack
+  //       v each section
+  // array<array<pair<index,index>>>
+  //             ^ indices for each cell in the section
+  // ^ all sections
+  constexpr static std::array section_table {
+  // {{{
+  // section 1,1
+    std::array {std::pair {0_z, 0_z},
+                std::pair {0_z, 1_z},
+                std::pair {0_z, 2_z},
+                std::pair {1_z, 0_z},
+                std::pair {1_z, 1_z},
+                std::pair {1_z, 2_z},
+                std::pair {2_z, 0_z},
+                std::pair {2_z, 1_z},
+                std::pair {2_z, 2_z}},
+ // section 1,2
+    std::array {std::pair {0_z, 3_z},
+                std::pair {0_z, 4_z},
+                std::pair {0_z, 5_z},
+                std::pair {1_z, 3_z},
+                std::pair {1_z, 4_z},
+                std::pair {1_z, 5_z},
+                std::pair {2_z, 3_z},
+                std::pair {2_z, 4_z},
+                std::pair {2_z, 5_z}},
+ // section 1,3
+    std::array {std::pair {0_z, 6_z},
+                std::pair {0_z, 7_z},
+                std::pair {0_z, 8_z},
+                std::pair {1_z, 6_z},
+                std::pair {1_z, 7_z},
+                std::pair {1_z, 8_z},
+                std::pair {2_z, 6_z},
+                std::pair {2_z, 7_z},
+                std::pair {2_z, 8_z}},
+ // section 2,1
+    std::array {std::pair {3_z, 0_z},
+                std::pair {3_z, 1_z},
+                std::pair {3_z, 2_z},
+                std::pair {4_z, 0_z},
+                std::pair {4_z, 1_z},
+                std::pair {4_z, 2_z},
+                std::pair {5_z, 0_z},
+                std::pair {5_z, 1_z},
+                std::pair {5_z, 2_z}},
+ // section 2,2
+    std::array {std::pair {3_z, 3_z},
+                std::pair {3_z, 4_z},
+                std::pair {3_z, 5_z},
+                std::pair {4_z, 3_z},
+                std::pair {4_z, 4_z},
+                std::pair {4_z, 5_z},
+                std::pair {5_z, 3_z},
+                std::pair {5_z, 4_z},
+                std::pair {5_z, 5_z}},
+ // section 2,3
+    std::array {std::pair {3_z, 6_z},
+                std::pair {3_z, 7_z},
+                std::pair {3_z, 8_z},
+                std::pair {4_z, 6_z},
+                std::pair {4_z, 7_z},
+                std::pair {4_z, 8_z},
+                std::pair {5_z, 6_z},
+                std::pair {5_z, 7_z},
+                std::pair {5_z, 8_z}},
+ // section 3,1
+    std::array {std::pair {6_z, 0_z},
+                std::pair {6_z, 1_z},
+                std::pair {6_z, 2_z},
+                std::pair {7_z, 0_z},
+                std::pair {7_z, 1_z},
+                std::pair {7_z, 2_z},
+                std::pair {8_z, 0_z},
+                std::pair {8_z, 1_z},
+                std::pair {8_z, 2_z}},
+ // section 3,2
+    std::array {std::pair {6_z, 3_z},
+                std::pair {6_z, 4_z},
+                std::pair {6_z, 5_z},
+                std::pair {7_z, 3_z},
+                std::pair {7_z, 4_z},
+                std::pair {7_z, 5_z},
+                std::pair {8_z, 3_z},
+                std::pair {8_z, 4_z},
+                std::pair {8_z, 5_z}},
+ // section 3,3
+    std::array {std::pair {6_z, 6_z},
+                std::pair {6_z, 7_z},
+                std::pair {6_z, 8_z},
+                std::pair {7_z, 6_z},
+                std::pair {7_z, 7_z},
+                std::pair {7_z, 8_z},
+                std::pair {8_z, 6_z},
+                std::pair {8_z, 7_z},
+                std::pair {8_z, 8_z}},
+  };
+  // }}}
+
+  for ( const auto& section : section_table ) {
+    for ( const auto& [row, col] : section ) {
+      if ( ! row_col_check_iteration(row, col) ) {
+        return false;
+      }
+    }
+
+    std::ranges::fill(check_table, false);
+  }
 
   return true;
 }
