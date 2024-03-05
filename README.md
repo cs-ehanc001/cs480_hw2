105356e13e318aa2ff52008e2f70c93c675c057a
Ethan Hancock <ehanc001@odu.edu>
2024-03-05 14:16:06 -0500

    main boilerplate and README

--- README.md
+++ README.md
@@ -12,4 +12,36 @@ The name of the resulting executable is `sudoku_solver`.
 
 If desired, unit tests can be run with the command: `cmake --build . --target test`
 
-## TODO: Running Instructions
+## Running Instructions
+
+The program takes two arguments: the search strategy, and the path to an input file.
+The search strategy must be either `--simple` or `--smart`.
+The format for the input file is described below.
+
+The program does accept a `--help` option
+
+## Input File Format
+
+Input files must take the form of 81 characters, separated by whitespace.
+Empty spaces are represented with `_`,
+and assigned cells are represented by their numerical value.
+File extension is not checked by the program.
+
+Below is the "easy" problem from the homework document in the appropriate format.
+
+The directory `inputs` contains the "easy," "medium," "hard," and "evil"
+puzzles from the homework document,
+as well as "trivial" and "already_solved" puzzles,
+along with an extra example.
+
+```
+_ 3 _ _ 8 _ _ _ 6
+5 _ _ 2 9 4 7 1 _
+_ _ _ 3 _ _ 5 _ _
+_ _ 5 _ 1 _ 8 _ 4
+4 2 _ 8 _ 5 _ 3 9
+1 _ 8 _ 3 _ 6 _ _
+_ _ 3 _ _ 7 _ _ _
+_ 4 1 6 5 3 _ _ 2
+2 _ _ _ 4 _ _ 6 _
+```
--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,14 +1,72 @@
+#include <cstdlib>
+#include <fstream>
 #include <iostream>
-#include <vector>
+#include <ranges>
+#include <string_view>
 
-#include <supl/utility.hpp>
+#include <supl/predicates.hpp>
 
-auto main() -> int
+#include "sudoku.hpp"
+
+void print_help_message([[maybe_unused]] const int argc,
+                        const char* const* const argv)
 {
-  std::cout << supl::stream_adapter {
-    std::vector {
-                 1, 2,
-                 3, 4,
-                 42}
-  } << '\n';
+  std::cerr << "Usage:\n"
+            << argv[0] << " --simple [input_file.dat]\n"
+            << argv[0] << " --smart [input_file.dat]\n";
+}
+
+auto main(const int argc, const char* const* const argv) -> int
+{
+  using namespace std::literals;  // for operator""sv string_view literal
+
+  for ( const int i : std::views::iota(1, argc) ) {
+    constexpr static auto help_pred {
+      supl::equals_any_of("--help"sv, "-h"sv)};
+    if ( help_pred(argv[i]) ) {
+      print_help_message(argc, argv);
+      return EXIT_SUCCESS;
+    }
+  }
+
+  if ( argc != 3 ) {
+    print_help_message(argc, argv);
+    return EXIT_FAILURE;
+  }
+
+  constexpr static auto strategy_pred {
+    supl::equals_any_of("--simple"sv, "--smart"sv)};
+  if ( ! strategy_pred(argv[1]) ) {
+    std::cerr << "Bad search strategy: \"" << argv[1]
+              << "\". Must be [--simple] or [--smart].\n";
+    print_help_message(argc, argv);
+    return EXIT_FAILURE;
+  }
+
+  const bool be_smart {"--smart"sv == argv[1]};
+
+  Sudoku sudoku {[argc, argv]() {
+    std::ifstream infile {argv[2]};
+
+    if ( ! infile.is_open() ) {
+      std::cerr << "Error opening file: \"" << argv[2] << "\"\n";
+      print_help_message(argc, argv);
+      std::exit(EXIT_FAILURE);
+    }
+    Sudoku retval;
+    infile >> retval;
+    return retval;
+  }()};
+
+  // below is placeholder
+
+  std::cout << sudoku << '\n';
+
+  if ( be_smart ) {
+    std::cout << "Solving smartly\n";
+  } else {
+    std::cout << "Solving simply\n";
+  }
+
+  return EXIT_SUCCESS;
 }
